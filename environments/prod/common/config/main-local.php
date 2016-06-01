b84fa878122e413833fd130ce85d77efcb77b7c2
drodata <drodata@foxmail.com>
2016-06-01 15:11:15 +0800

    Enh: config prod common env

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,16 +1,44 @@
 <?php
+$arg = [
+    'password' => '',
+    'domain' => 'drodata.com',
+    'dbname' => 'drodata_com',
+];
 return [
+    'aliases' => [
+        '@epweb'  => 'http://ep.' . $arg['domain'],
+        '@iepweb'  => 'http://i.ep.' . $arg['domain'],
+    ],
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=' . $arg['dbname'],
             'username' => 'root',
-            'password' => '',
+            'password' => $arg['password'],
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'user' => [
+            'class' => 'yii\web\User',
+            'identityCookie' => [
+                'name' => '_identity',
+                'domain'   => '.' .$arg['domain'],
+                'httpOnly' => true,
+            ],
+        ],
+        'session' => [
+            'class' => 'yii\web\Session',
+            'cookieParams' => [
+                'domain'   => '.' .$arg['domain'],
+                'httpOnly' => true,
+            ],
         ],
     ],
 ];
