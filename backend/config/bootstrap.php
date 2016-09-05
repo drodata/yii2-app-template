a0ef11ef20fa456870b7c8756bf4a60929ebeaa2
drodata <drodata@foxmail.com>
2016-09-05 08:28:34 +0800

    fix: typo

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 Yii::$container->set('yii\grid\GridView', [
-    'options' => ['class' => 'table-responseive grid-view'],
+    'options' => ['class' => 'table-responsive grid-view'],
     'tableOptions' => [
         'class' => 'table table-striped table-hover',
     ],
