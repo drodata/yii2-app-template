74b741d4ff6720f5bea57270139797480feac799
drodata <drodata@foxmail.com>
2016-11-16 14:48:43 +0800

    twk: site/error layout

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -5,23 +5,18 @@
 /* @var $message string */
 /* @var $exception Exception */
 
-use yii\helpers\Html;
+use drodata\helpers\Html;
+use drodata\widgets\Box;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="row">
+    <div class="col-lg-6 col-lg-offset-3 col-md-12">
+        <?= Box::widget([
+            'title' => Html::icon('warning'),
+            'style' => 'danger',
+            'solid' => true,
+            'content' => $message
+        ]) ?>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
