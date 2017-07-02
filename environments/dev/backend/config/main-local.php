8e7e4e43f78daa6b3d42cefb7656494cbb87bbd4
drodata <drodata@foxmail.com>
2017-07-02 20:53:13 +0800

    Enh: dev environment, add local utility alias #10

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -22,6 +22,9 @@ if (!YII_ENV_TEST) {
     ];
 }
 if (YII_ENV_DEV) {    
+    // to debug the yii2-utility extension locally
+    $config['aliases']['@ut'] = 'path-to-local-yii2-utility';
+
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',      
         'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*', '192.168.178.20'],  
@@ -30,24 +33,28 @@ if (YII_ENV_DEV) {
                 'class' => 'yii\gii\generators\crud\Generator',
                 'templates' => [
                     'drodata' => '@drodata/gii-templates/backend/crud/default',
+                    'drodata-local' => '@ut/gii-templates/backend/crud/default',
                 ]
             ],
             'model' => [
                 'class' => 'yii\gii\generators\model\Generator',
                 'templates' => [
                     'drodata' => '@drodata/gii-templates/backend/model/default',
+                    'drodata-local' => '@ut/gii-templates/backend/model/default',
                 ]
             ],
             'controller' => [
                 'class' => 'yii\gii\generators\controller\Generator',
                 'templates' => [
                     'drodata' => '@drodata/gii-templates/backend/controller/default',
+                    'drodata-local' => '@ut/gii-templates/backend/controller/default',
                 ]
             ],
             'form' => [
                 'class' => 'yii\gii\generators\form\Generator',
                 'templates' => [
                     'drodata' => '@drodata/gii-templates/backend/form/default',
+                    'drodata-local' => '@ut/gii-templates/backend/form/default',
                 ]
             ],
         ],
