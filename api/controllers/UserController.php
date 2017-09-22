bae8bebfaf6e40a78c917f092f488be86f3b8b3f
drodata <drodata@foxmail.com>
2017-09-22 14:09:11 +0800

    增加 api user 鉴权行为 #8

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -4,12 +4,23 @@ namespace api\controllers;
 
 use yii\rest\ActiveController;
 use yii\data\ActiveDataProvider;
+use yii\filters\auth\QueryParamAuth;
 use common\models\User;
 
 class UserController extends ActiveController
 {
     public $modelClass = 'common\models\User';
 
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['authenticator'] = [
+            'class' => QueryParamAuth::className(),
+        ];
+
+        return $behaviors;
+    }
     public function actionIndex()
     {
         $query = User::find()->joinWith(['group']);
