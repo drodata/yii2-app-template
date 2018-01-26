f738db8487d4747561c3fdc723d16db7e0a25085
drodata <drodata@foxmail.com>
2018-01-26 09:13:29 +0800

    改进 环境配置, 开发阶段使用自定义的 migration 模板文件

--- environments/dev/console/config/main-local.php
+++ environments/dev/console/config/main-local.php
@@ -1,6 +1,13 @@
 <?php
 return [
     'bootstrap' => ['gii'],
+    'controllerMap' => [
+        'migrate' => [
+            'class' => 'yii\console\controllers\MigrateController',
+            // 使用 drodata/yii2-utility 中的模板文件
+            'templateFile' => '@drodata/views/migration.php',
+        ],
+    ],
     'modules' => [
         'gii' => 'yii\gii\Module',
     ],
