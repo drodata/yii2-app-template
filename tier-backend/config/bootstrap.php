bca60e329721a3f1ff2a4e5e15d69a77da8dc732
drodata <drodata@foxmail.com>
2018-04-12 21:23:35 +0800

    全局 实现 GridView 列宽自适应

--- tier-backend/config/bootstrap.php
+++ tier-backend/config/bootstrap.php
@@ -2,7 +2,7 @@
 Yii::$container->set('yii\grid\GridView', [
     'options' => ['class' => 'table-responsive grid-view'],
     'tableOptions' => [
-        'class' => 'table table-striped table-hover',
+        'class' => 'table table-striped table-hover table-nowrap',
     ],
 ]);
 
