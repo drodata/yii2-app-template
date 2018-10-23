4159b0fa18380e92fd944e100bd4510a3acfa0ec
drodata <drodata@foxmail.com>
2018-10-23 10:01:46 +0800

    改进 common cofig 文件，增加缺失的 static alias

--- common/config/main.php
+++ common/config/main.php
@@ -1,8 +1,14 @@
 <?php
+$sensitive = json_decode(file_get_contents(Yii::getAlias('@common') . '/yii2-sensitive.json'));
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'language' => 'zh-CN',
     'timeZone' => 'Asia/Shanghai', // timezone list: http://php.net/manual/en/timezones.php
+    'aliases' => [
+        '@staticweb'  => 'http://static.' . $sensitive->domain,
+        '@frontendweb'  => 'http://www.' . $sensitive->domain,
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
